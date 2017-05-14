73071dacaa33671b4973fb97a6b1e5d553196a57
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-05-13 22:42:04 -0300

    UART works and Pure Data communication was established

--- src/UART.c
+++ src/UART.c
@@ -35,5 +35,6 @@ void USART_config()
 
 void send_data(char data)
 {
+	while(!((USART3->SR & USART_SR_TXE) ==  USART_SR_TXE));
 	USART3->DR = data;
 }
--- src/main.c
+++ src/main.c
@@ -4,7 +4,29 @@
 #include "UART.h"
 			
 uint16_t result = 0;
-char data = 'a';
+char data = 'b';
+char converted_data[4];
+uint16_t average_result = 0;
+uint16_t filtered_result = 0;
+
+void convert_data(uint16_t value)
+{
+	converted_data[0] = value/1000;
+	converted_data[1] = (value/100 - converted_data[0]*10);
+	converted_data[2] = (value/10 - (converted_data[0]*100) - (converted_data[1]*10));
+	converted_data[3] = (value - (converted_data[0]*1000) - (converted_data[1]*100) - (converted_data[2]*10));
+	for (int i = 0; i < 4; ++i)
+	{
+		converted_data[i] += 48;
+	}
+
+}
+
+
+
+
+
+
 int main(void)
 {
 	ClockConfig();
@@ -16,11 +38,21 @@ int main(void)
 	adc_config();
 	for(;;)
 	{
-		result = adc_value();
-
-		if ((USART3->SR & USART_SR_TXE) ==  USART_SR_TXE)
+		for (int i = 0; i < 20; ++i)
 		{
-			send_data(data);
+			result = adc_value();
+			average_result += result;
 		}
+		average_result  = average_result/20;
+		filtered_result = filtered_result*0.0001 + average_result;
+		convert_data(filtered_result);
+		send_data('b');
+		send_data(' ');
+		send_data(converted_data[0]);
+		send_data(converted_data[1]);
+		send_data(converted_data[2]);
+		send_data(converted_data[3]);
+		send_data('\n');
+		send_data('\r');
 	}
 }
