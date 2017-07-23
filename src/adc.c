5c3bae6086a4cd6a0dc9d2b8bb96594361335dee
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-07-22 21:23:47 -0300

    16 ADC channels working

--- src/adc.c
+++ src/adc.c
@@ -11,46 +11,70 @@
 
 
 
-// TO DO : add all the 16 channels and test them   1:config gpio  2:config sample time  3: set number of conversions 4: set order of conversion
+// TO DO :  1:config gpio  2:config sample time  3: set number of conversions 4: set order of conversion
 
 void ADCclock_config() //              /to do -> add parameters for adc and gpio selection
 {
 	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // enables ADC1 clock
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // enables GPIOA which contains ADC1_channel 1
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
 }
 
 void GPIOx_config() //       /to do -> add parameters for which gpio to config and types of configuration
 {
 
-	GPIOB->MODER |= GPIO_MODER_MODER0; 
-	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPDR0);
+	//MODERx[1,0] = 11 for analog in pin x /see GPIO_MODER page 283 manual  
+	// PUPDRx[1,0] = 00 for analog in pin x /see GPIO_PUPDR page 284 manual 
 
+	//ADC1_CH0 -> PA0
 	GPIOA->MODER |= GPIO_MODER_MODER0; 
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR0);
-
 	//ADC1_CH1 -> PA1
-	//to select the gpio parameters(input,output,pull-up,analog...) 4 registers must be configured
-	GPIOA->MODER |= 0xC; //MODERx[1,0] = 11 for analog in pin x /see GPIO_MODER page 283 manual / to do -> change for mask
-	GPIOA->PUPDR &= 0x00; // PUPDRx[1,0] = 00 for analog in pin x /see GPIO_PUPDR page 284 manual / to do -> change for mask
+	GPIOA->MODER |= GPIO_MODER_MODER1;
+	GPIOA->PUPDR &=  ~(GPIO_PUPDR_PUPDR1);
 	//ADC1_CH2 -> PA2
 	GPIOA->MODER |= GPIO_MODER_MODER2; 
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR2);
 	//ADC1_CH3 -> PA3
 	GPIOA->MODER |= GPIO_MODER_MODER3;
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR3);
-
+	//ADC1_CH4 -> PA4
 	GPIOA->MODER |= GPIO_MODER_MODER4;
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR4);
-
+	//ADC1_CH5 -> PA5
 	GPIOA->MODER |= GPIO_MODER_MODER5;
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR5);
-
+	//ADC1_CH6 -> PA6
 	GPIOA->MODER |= GPIO_MODER_MODER6;
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR6);
-
+	//ADC1_CH7 -> PA7
 	GPIOA->MODER |= GPIO_MODER_MODER7;
 	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR7);
+	//ADC1_CH8 -> PB0
+	GPIOB->MODER |= GPIO_MODER_MODER0; 
+	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPDR0);
+	//ADC1_CH9 -> PB1
+	GPIOB->MODER |= GPIO_MODER_MODER1; 
+	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPDR1);
+	//ADC1_CH10 -> PC0
+	GPIOC->MODER |= GPIO_MODER_MODER0; 
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR0);
+	//ADC1_CH11 -> PC1
+	GPIOC->MODER |= GPIO_MODER_MODER1; 
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR1);
+	//ADC1_CH12 -> PC2
+	GPIOC->MODER |= GPIO_MODER_MODER2; 
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR2);
+	//ADC1_CH13 -> PC3
+	GPIOC->MODER |= GPIO_MODER_MODER3; 
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR3);
+	//ADC1_CH14 -> PC4
+	GPIOC->MODER |= GPIO_MODER_MODER4; 
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR4);
+	//ADC1_CH15 -> PC5
+	GPIOC->MODER |= GPIO_MODER_MODER5; 
+	GPIOC->PUPDR &= ~(GPIO_PUPDR_PUPDR5);
 
 
 }
@@ -96,70 +120,82 @@ void adc_config_multi() //       /to do -> add parameters for types of configura
 	ADC1->CR2 |= 0<<28;
 	ADC1->CR2 |= 0<<29;
 
-	ADC1->SQR1 |= ADC_SQR1_L_3;//(ADC_SQR1_L_2|ADC_SQR1_L_1| ADC_SQR1_L_0); // set number of conversions to 8 (ADC_SQR1_L_3| ADC_SQR1_L_0);
-
+	// ------------Number of Conversions--------------
+	// 16 conversions
+	ADC1->SQR1 |= ADC_SQR1_L;
+	//------------------------------------------------
 	// sampling time configuration (000) -> 3 cycles see page 423 manual 
 	ADC1->SMPR2 &= SMPR2_SMP3_RESET;
 
 	//----------Sample Time-----------------
 	// 15 cycles CH0
 	ADC1->SMPR2 |= ADC_SMPR2_SMP0_0;
-
 	// 15 cycles CH1
 	ADC1->SMPR2 |= ADC_SMPR2_SMP1_0;
-
 	// 15 cycles CH2
 	ADC1->SMPR2 |= ADC_SMPR2_SMP2_0;
-	
 	// 15 cycles CH3
 	ADC1->SMPR2 |= ADC_SMPR2_SMP3_0;
-
 	// 15 cycles CH4
 	ADC1->SMPR2 |= ADC_SMPR2_SMP4_0;
-
 	//15 cycles CH5
 	ADC1->SMPR2 |= ADC_SMPR2_SMP5_0;
-
 	//15 cycles CH6
 	ADC1->SMPR2 |= ADC_SMPR2_SMP6_0;
-
 	//15 cycles CH7
 	ADC1->SMPR2 |= ADC_SMPR2_SMP7_0;
-
 	// 15 cycles CH8
 	ADC1->SMPR2 |= ADC_SMPR2_SMP8_0;
+	// 15 cycles CH9
+	ADC1->SMPR2 |= ADC_SMPR2_SMP9_0;
+	// 15 cycles CH10
+	ADC1->SMPR1 |= ADC_SMPR1_SMP10_0;
+	// 15 cycles CH11
+	ADC1->SMPR1 |= ADC_SMPR1_SMP11_0;
+	// 15 cycles CH12
+	ADC1->SMPR1 |= ADC_SMPR1_SMP12_0;
+	// 15 cycles CH13
+	ADC1->SMPR1 |= ADC_SMPR1_SMP13_0;
+	// 15 cycles CH14
+	ADC1->SMPR1 |= ADC_SMPR1_SMP14_0;
+	// 15 cycles CH15
+	ADC1->SMPR1 |= ADC_SMPR1_SMP15_0;
 
 	//----------Conversion Order----------------------------
 
 	//CH0 first conversion
 	ADC1->SQR3 &= ~(ADC_SQR3_SQ1);
-
 	//CH1 second conversion
 	ADC1->SQR3 |= ADC_SQR3_SQ2_0;
-
 	//CH2 third conversion
 	ADC1->SQR3 |= ADC_SQR3_SQ3_1;
-
 	//CH3 fourth conversion
 	ADC1->SQR3 |= (ADC_SQR3_SQ4_1|ADC_SQR3_SQ4_0);
-
 	// CH4 fifth conversion
 	ADC1->SQR3 |= ADC_SQR3_SQ5_2;
-
 	// CH5 sixth conversion
 	ADC1->SQR3 |= (ADC_SQR3_SQ6_2|ADC_SQR3_SQ6_0);
-
 	// CH6 seventh conversion
 	ADC1->SQR2 |= (ADC_SQR2_SQ7_2|ADC_SQR2_SQ7_1);
-
 	//CH7 eighth conversion
 	ADC1->SQR2 |= (ADC_SQR2_SQ8_2|ADC_SQR2_SQ8_1|ADC_SQR2_SQ8_0);
-
 	// CH8 ninth conversion
 	ADC1->SQR2 |= ADC_SQR2_SQ9_3;
-
-	//ADC1->SQR2 &= ~(ADC_SQR2_SQ8);
-	 //------------------------------------------------------
+	// CH9 tenth conversion
+	ADC1->SQR2 |= (ADC_SQR2_SQ10_3|ADC_SQR2_SQ10_0);
+	// CH10 eleventh conversion
+	ADC1->SQR2 |= (ADC_SQR2_SQ11_3|ADC_SQR2_SQ11_1);
+	// CH11 twelfth conversion
+	ADC1->SQR2 |= (ADC_SQR2_SQ12_3|ADC_SQR2_SQ12_1|ADC_SQR2_SQ12_0);
+	// CH12 thirteenth conversion
+	ADC1->SQR1 |= (ADC_SQR1_SQ13_3|ADC_SQR1_SQ13_2);
+	// CH13 fourteenth conversion
+	ADC1->SQR1 |= (ADC_SQR1_SQ14_3|ADC_SQR1_SQ14_2|ADC_SQR1_SQ14_0);
+	// CH14 fifteennth conversion
+	ADC1->SQR1 |= (ADC_SQR1_SQ15_3|ADC_SQR1_SQ15_2|ADC_SQR1_SQ15_1);
+	// CH15 sixteenth conversion
+	ADC1->SQR1 |= (ADC_SQR1_SQ16_3|ADC_SQR1_SQ16_2|ADC_SQR1_SQ16_1|ADC_SQR1_SQ16_0);
+	//------------------------------------------------------------------------------
 
 
 
--- src/main.c
+++ src/main.c
@@ -6,15 +6,16 @@
 #include "I2C.h"
 #include "LSM9DS1.h"
 			
+			// TO DO: filter function /calibrate LSM9DS1 /change i2c to interrupt/ use madwick algorithm
 
 #define LSM9DS1_AG_ADDR  0x6B
 int8_t adc_counter = 0;
 uint16_t result = 0;
 char data = 'b';
 uint32_t average_result = 0;
-volatile int32_t filtered_flex[] = {0x0, 0x0};
-volatile int32_t output_data[] = {0x0, 0x0};
-volatile int16_t buffer[8];
+volatile int32_t filtered_flex[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+volatile int32_t output_data[] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+volatile int16_t buffer[13];
 volatile int32_t flex_data[] = {0x0, 0x0, 0x0, 0x0};
 volatile int count = 0;
 uint16_t flag = 0;
@@ -26,12 +27,15 @@ volatile uint8_t systick_flag = 0;
 
 
 
+
 void DMA2_Stream0_IRQHandler()
 {
 	if (((DMA2 -> LISR)&(DMA_LISR_TCIF0)) == DMA_LISR_TCIF0 )
 	{
 		systick_flag = 0;
-		DMA2 -> LIFCR |= DMA_LIFCR_CTCIF0;	
+		DMA2 -> LIFCR |= DMA_LIFCR_CTEIF0;
+		DMA2 -> LIFCR |= DMA_LIFCR_CTCIF0;
+		DMA2 -> LIFCR |= DMA_LISR_HTIF0;
 	}
 }
 
@@ -45,7 +49,6 @@ void SysTick_Handler()
 		ADC1->CR2 |= ADC_CR2_SWSTART; 
 		systick_flag = 1;
 	}
-		//ADC1->CR2 |= ADC_CR2_ADON; // activate ADC
 }
 
 void ADC_IRQHandler()
@@ -53,10 +56,12 @@ void ADC_IRQHandler()
 	if ((ADC1->SR & ADC_SR_OVR) == ADC_SR_OVR)
 	{
 		DMA2_Stream0 -> M0AR |= (uint32_t)buffer;
-		DMA2_Stream0 -> NDTR |= 0x8;
+		DMA2_Stream0 -> NDTR |= 0x10;
 		ADC1->SR &= ~(ADC_SR_OVR);
-		dummy++;
+		ADC1->CR2 &= ~(ADC_CR2_DMA);
+		ADC1->CR2 |= ADC_CR2_DMA;
 		ADC1->CR2 |= ADC_CR2_SWSTART;
+		systick_flag = 1;
 	}
 	/*else if ((ADC1->SR & ADC_SR_EOC) == ADC_SR_EOC)
 	{
@@ -75,20 +80,20 @@ void ADC_IRQHandler()
 
 int main(void)
 {
-	(void)SysTick_Config(0x19A280);
+	(void)SysTick_Config(0x334500); //334500 0x19A280
 	ClockConfig();
 	USARTclock_config();
-	I2C_clock_init();
-	I2C_gpio_config();
-	I2C_config(I2C2);
-	LSM9DS1_init();
+	//I2C_clock_init();
+	//I2C_gpio_config();
+	//I2C_config(I2C2);
+	//LSM9DS1_init();
 	GPIO_config();
 	USART_config();
-	NVIC_SetPriority(ADC_IRQn, 1);
+	NVIC_SetPriority(ADC_IRQn, 2);
 	NVIC_EnableIRQ(ADC_IRQn);
 	ADCclock_config();
 	GPIOx_config();
-	NVIC_SetPriority(DMA2_Stream0_IRQn, 2);
+	NVIC_SetPriority(DMA2_Stream0_IRQn, 1);
 	NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 	DMA_config();
 	DMA2_Stream0 -> M0AR |= (uint32_t)buffer;
@@ -102,17 +107,32 @@ int main(void)
 		//dummy = DWT->CYCCNT;
 		//I2C_test = I2C_Read(I2C2, LSM9DS1_AG_ADDR, 0x20);
 		//accel_test = I2C_Read(I2C2, LSM9DS1_AG_ADDR, 0x27);
-		if (accel_available())
+		/*if (accel_available())
 		{
 			count++;
 			accel_read(accel_data);
-		}
+		}*/
 		// change this stuff to a function 
 		filtered_flex[0] = filtered_flex[0] + ((buffer[0] - filtered_flex[0])>>4);
 		output_data[0] = output_data[0] + ((filtered_flex[0] - output_data[0])>>4);
 
 		filtered_flex[1] = filtered_flex[1] + ((buffer[1] - filtered_flex[1])>>4);
 		output_data[1] = output_data[1] + ((filtered_flex[1] - output_data[1])>>4);
+
+		filtered_flex[2] = filtered_flex[2] + ((buffer[2] - filtered_flex[2])>>4);
+		output_data[2] = output_data[2] + ((filtered_flex[2] - output_data[2])>>4);
+
+		filtered_flex[3] = filtered_flex[3] + ((buffer[3] - filtered_flex[3])>>4);
+		output_data[3] = output_data[3] + ((filtered_flex[3] - output_data[3])>>4);
+
+		filtered_flex[4] = filtered_flex[4] + ((buffer[4] - filtered_flex[4])>>4);
+		output_data[4] = output_data[4] + ((filtered_flex[4] - output_data[4])>>4);
+
+		filtered_flex[5] = filtered_flex[5] + ((buffer[5] - filtered_flex[5])>>4);
+		output_data[5] = output_data[5] + ((filtered_flex[5] - output_data[5])>>4);
+
+		filtered_flex[6] = filtered_flex[6] + ((buffer[6] - filtered_flex[6])>>4);
+		output_data[6] = output_data[6] + ((filtered_flex[6] - output_data[6])>>4);
 		//DWT->CYCCNT = 0;
 		/*send_data('x');
 		send_data(' ');
@@ -126,14 +146,35 @@ int main(void)
 		send_data(' ');
 		print_data((int32_t)accel_data[2]);
 		send_data(' ');*/
-		send_data('r');
+		//send_data('\n');
+		//send_data('\r');
+		send_data('0');
 		send_data(' ');
 		print_data(output_data[0]);
-		send_data('\n');
-		send_data('\r');
-		send_data('s');
+		send_data(' ');
+		send_data('1');
 		send_data(' ');
 		print_data(output_data[1]);
+		send_data(' ');
+		send_data('2');
+		send_data(' ');
+		print_data(output_data[2]);
+		send_data(' ');
+		send_data('3');
+		send_data(' ');
+		print_data(output_data[3]);
+		send_data(' ');
+		send_data('4');
+		send_data(' ');
+		print_data(output_data[4]);
+		send_data(' ');
+		send_data('5');
+		send_data(' ');
+		print_data(output_data[5]);
+		send_data(' ');
+		send_data('6');
+		send_data(' ');
+		print_data(output_data[6]);
 		send_data('\n');
 		send_data('\r');
 		count++;
